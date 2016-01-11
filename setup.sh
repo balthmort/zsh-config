9248e552b206f4e2f137ee43ecefa1f88bf4e903
balthmort <balthmort@gmail.com>
2016-01-11 21:47:01 +0100

    Redundant (probably) setup removed

--- setup.sh
+++
@@ -1,5 +0,0 @@
-# if in empty dir, clone self
-test -z "$(ls .)" && git clone --recursive https://github.com/balthmort/zsh-config.git .
-
-# link zshrc
-test -f "$HOME/.zshrc" || ln -s "$PWD/zshrc" "$HOME/.zshrc"
