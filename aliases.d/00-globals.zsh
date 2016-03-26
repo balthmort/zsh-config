330e1cec1762444ad39dc96bb76ae9725a9e064f
balthmort <balthmort@gmail.com>
2016-03-26 01:56:17 +0100

    zsh upstrem fetch

--- aliases.d/00-globals.zsh
+++ aliases.d/00-globals.zsh
@@ -51,9 +51,6 @@ function man
   man "$@"
 }
 
-#Pip packages update
-alias update-pip="pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 sudo pip install -U"
-
 # Suffix aliases
 alias -s php='php -f'
 alias -s pdf='zathura --fork'
